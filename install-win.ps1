07ff6d37aa7fb94913c91c1c9ef3a78c51ae524b
heshsaih <heshsaih@gmail.com>
2025-10-08 18:11:19 +0200

    feat: more advanced install script, it now checks for  all of the necessary cmd tools and installs them if missing

--- install-win.ps1
+++ install-win.ps1
@@ -12,12 +12,47 @@ function ln ($target, $link)
     }
 }
 
-# script expects that you have the wezterm emulator installed
+# checking if there are necessary tools installed
 Get-Command wezterm
 if (!$?)
 {
     Write-Output "wezterm not available, exiting"
     Exit 0    
+}
+
+Get-Command scoop
+if (!$?)
+{
+    Write-Output "scoop not installed, installing"
+    Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
+    Invoke-RestMethod -Uri https://get.scoop.sh | Invoke-Expression
+    if (!$?)
+    {
+        Write-Output "failed to install scoop, exiting"
+        Exit 0
+    }
+}
+Get-Command fd
+if (!$?)
+{
+    Write-Output "fd not installed, installing"
+    scoop install fd
+    if (!$?)
+    {
+        Write-Output "failed to install fd, exiting"
+        Exit 0
+    }
+}
+Get-Command fzf
+if (!$?)
+{
+    Write-Output "fzf not installed, installing"
+    scoop install fzf
+    if (!$?)
+    {
+        Write-Output "failed to install fzf, exiting"
+        Exit 0
+    }
 }
 
 ln "$PWD\wezterm\.wezterm.lua" "$HOME\.wezterm.lua"
