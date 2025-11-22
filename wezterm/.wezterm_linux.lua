cafcc164fb870c21b695099ba8360072eeffe526
heshsaih <heshsaih@gmail.com>
2025-11-22 22:35:59 +0100

    separate wezterm configs for linux and windows

---
+++ wezterm/.wezterm_linux.lua
@@ -0,0 +1,46 @@
+local wezterm = require("wezterm")
+local config = wezterm.config_builder()
+local mux = wezterm.mux
+
+config.color_scheme = "rose-pine"
+config.default_prog = {"/usr/bin/zsh"}
+config.window_background_opacity = 1
+
+config.window_decorations = "TITLE | RESIZE"
+wezterm.on("gui-startup", function()
+	local _, _, window = mux.spawn_window({})
+	window:gui_window():maximize()
+end)
+
+config.font = wezterm.font("JetBrains Mono", {
+	weight = "Medium",
+})
+config.cell_width = 1
+config.font_size = 13
+
+local bar = wezterm.plugin.require("https://github.com/adriankarlen/bar.wezterm")
+bar.apply_to_config(config, {
+	position = "bottom",
+	padding = {
+		tabs = {
+			left = 2,
+			right = 2,
+		},
+	},
+	modules = {
+		workspace = {
+			enabled = false,
+		},
+		pane = {
+			enabled = false,
+		},
+		hostname = {
+			enabled = false,
+		},
+		cwd = {
+			enabled = false,
+		},
+	},
+})
+
+return config
