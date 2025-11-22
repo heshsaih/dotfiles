9736ad113b7a576889cab71390af1c7ec07c7dba
heshsaih <heshsaih@gmail.com>
2025-11-22 22:07:42 +0100

    i dont really know some stuff

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -27,6 +27,7 @@ return {
       require("mason-lspconfig").setup({
         ensure_installed = languages,
       })
+      lspconfig.gdscript.setup(capabilities)
       vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
       vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
       vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,22 +1,16 @@
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
-local mux = wezterm.mux
 
-config.color_scheme = "Gruvbox Dark (Gogh)"
+config.color_scheme = "rose-pine"
 config.default_prog = { "C:/Program Files/PowerShell/7/pwsh.exe -nologo" }
 config.window_background_opacity = 1
 
 config.window_decorations = "TITLE | RESIZE"
-wezterm.on("gui-startup", function()
-	local _, _, window = mux.spawn_window({})
-	window:gui_window():maximize()
-end)
-
 config.font = wezterm.font("JetBrains Mono", {
 	weight = "Medium",
 })
 config.cell_width = 1
-config.font_size = 13
+config.font_size = 17
 
 local bar = wezterm.plugin.require("https://github.com/adriankarlen/bar.wezterm")
 bar.apply_to_config(config, {
